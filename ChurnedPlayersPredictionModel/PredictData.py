79576abc0140ca25053869c892365c5734adf2d1
Nickel-nc <nickel-nc@inbox.ru>
2020-04-06 14:16:31 +0300

    remove during structure update

--- ChurnedPlayersPredictionModel/PredictData.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from Settings import *
-
-def get_predict(model, X_test):
-
-    y_pred = model.predict(X_test)
-    X_test['is_churned'] = y_pred
-    X_test.to_csv(OUTPUT_DATA_PATH)
-    print(f'Prediction succesfully saved to {OUTPUT_DATA_PATH}')
